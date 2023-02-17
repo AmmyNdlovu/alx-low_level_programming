fcf84e3af7c7d632daa11fe63d5814bd1adec918
amanda <ndlovuamanda89@gmail.com>
2023-02-16 18:50:37 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 
 	long long int d;
 
-	float f
+	float f;
 
 		printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 		printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
